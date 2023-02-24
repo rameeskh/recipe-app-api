699b31c78faa220946df8d731909e1799c84dabc
Ramees H <khramees.ramees@gmail.com>
2023-02-24 11:21:07 +0530

    implemented test case for wait_for_db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,3 +11,14 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands"""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """test waiting for database if database is ready"""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(database=['default'])
