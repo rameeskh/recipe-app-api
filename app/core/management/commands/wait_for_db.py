2fb03376a28425465bdf2695e5860054a90c4ae2
Ramees H <khramees.ramees@gmail.com>
2023-02-24 10:51:06 +0530

    created wait_for_db custom command file created a test file for the db_wait commad.

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Django command  to wait for the database to be available
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for the database"""
+
+    def handle(self, *args, **options):
+        pass
