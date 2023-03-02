52d1bd1f590ac840f224ca1c9e11055613bd0247
Ramees H <khramees.ramees@gmail.com>
2023-03-02 12:48:04 +0530

    changed from assertEqual to assertTrue for checkpasword

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -145,5 +145,5 @@ class PrivateUserApiTests(TestCase):
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
-        self.assertEqual(self.user.check_password(payload['password']))
+        self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
