39bdbfada3b177cc06eee17e01a192daac094caa
Ramees H <khramees.ramees@gmail.com>
2023-03-02 09:34:12 +0530

    created test for creation of token with bad creadentials created test for creation of token with blank password

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -83,4 +83,20 @@ class PublicUserApiTests(TestCase):
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-    
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials are invalid"""
+        create_user(email='test@example.com', password='goodpass')
+
+        payload = {'email': 'test@example.com', 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test posts a blank password returns an error"""
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
