107c03dd3653a2c394126001f763d3e7d38e9508
Ramees H <khramees.ramees@gmail.com>
2023-03-03 16:52:19 +0530

    created recipeserializer

---
+++ app/recipe/serializers.py
@@ -0,0 +1,16 @@
+"""
+Serializers for Recipe APIs
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for Recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+        
