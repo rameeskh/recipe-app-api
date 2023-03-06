1bfd8c550166610cda940d1a631c76b40fc6e71b
Ramees H <khramees.ramees@gmail.com>
2023-03-06 17:24:02 +0530

    added few more tests for recipe apis

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -159,7 +159,7 @@ class PrivateRecipeApiTests(TestCase):
         for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
         self.assertEqual(recipe.user, self.user)
-
+  
     def test_update_user_returns_error(self):
         """Test changing the recipe user results in an error"""
         new_user = create_user(email='user2@example.com', password='test124')
@@ -171,3 +171,24 @@ class PrivateRecipeApiTests(TestCase):
         
         recipe.refresh_from_db()
         self.assertEqual(recipe.user, self.user)
+
+    def test_delete_recipe(self):
+        """Test deleting a recipe successful"""
+        recipe = create_recipe(user=self.user)
+
+        url = detail_url(recipe.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Recipe.objects.filter(id=recipe.id).exists())
+
+    def test_delete_other_user_recipe_error(self):
+        """Test trying to delete other users recipe gives error"""
+        new_user = create_user(email='user2@example.com', password='test124')
+        recipe = create_recipe(user=new_user)
+
+        url = detail_url(recipe.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue(Recipe.objects.filter(id=recipe.id).exists())
