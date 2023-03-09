9a6376c9947a3d234228de21e778f60636fbf879
Ramees H <khramees.ramees@gmail.com>
2023-03-09 16:37:33 +0530

    created test for deleting ingredient Implemted delete ingredient API

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -83,3 +83,13 @@ class PrivateIngredientApiTests(TestCase):
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload['name'])
 
+    def test_delete_ingredient(self):
+        """test deleting an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name='Banana')
+
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())
