94e118fc59875b8aadfab59a8f6cd11902d63d6a
Ramees H <khramees.ramees@gmail.com>
2023-03-17 12:14:09 +0530

    updated requirements Updated Docker file updated run.sh

--- Dockerfile
+++ Dockerfile
@@ -35,7 +35,7 @@ RUN python -m venv /py && \
     chmod -R 755 /vol && \
     chmod -R +x /scripts
 
-ENV PATH="/py/bin:$PATH"
+ENV PATH="/scripts:/py/bin:$PATH"
 
 USER django-user
 
--- scrpits/run.sh
+++ scrpits/run.sh
@@ -6,3 +6,4 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
