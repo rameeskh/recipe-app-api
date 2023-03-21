3ad2f8a894e0e42ca6b35106498abec1557db685
RAMEES K H <khramees.ramees@gmail.com>
2023-03-21 12:27:36 +0530

    debug varible made to retrieve from environment variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,9 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
