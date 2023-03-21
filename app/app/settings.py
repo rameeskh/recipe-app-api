71912888655b671022fbb74014a09dc0e68a1ad3
RAMEES K H <khramees.ramees@gmail.com>
2023-03-21 12:20:47 +0530

    added secret key retrieving from environment variable

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ar&=e=j5n@)tn-gc*43+rzfta(t3qe0&c-_(xb5f7s_e(t3mx'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
